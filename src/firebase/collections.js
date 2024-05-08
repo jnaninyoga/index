bf4261810bf563ba8b1d558698e4476dbafeb873
vvhybe <vvhybe@hotmail.com>
2024-05-08 11:46:56 +0100

    Changing the DB Collection entries deplacment

--- src/firebase/collections.js
+++ src/firebase/collections.js
@@ -3,21 +3,22 @@ import { collectionDB } from ".";
 export const names = {
     auth: import.meta.env.VITE_COLLECTION_AUTH,
     books: import.meta.env.VITE_COLLECTION_BOOKS,
-    carnets: import.meta.env.VITE_COLLECTION_CARNETS,
     classes: import.meta.env.VITE_COLLECTION_CLASSES,
     clients: import.meta.env.VITE_COLLECTION_CLIENTS,
     configurations: import.meta.env.VITE_COLLECTION_CONFIGURATIONS,
     contacts: import.meta.env.VITE_COLLECTION_CONTACTS,
     emails: import.meta.env.VITE_COLLECTION_EMAILS,
     reviews: import.meta.env.VITE_COLLECTION_REVIEWS,
-    // collection configurations:
 }
 
+
+// collection configurations:
 export const configurations = {
-    carnets: import.meta.env.VITE_COLLECTION_CARNETS,
+    carnets: import.meta.env.VITE_CONIG_COLLECTION_CARNETS,
 }
 
 export const collections = Object.keys(names).reduce((collection, name) => {
+    // console.table(names); //debug the db collection entries
     collection[name] = collectionDB(names[name]);
     return collection;
 }, {});
